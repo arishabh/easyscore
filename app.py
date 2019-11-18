cd288bb582251469bdd23aa389b0447109472f20
rishabhagrawal41 <rishabhagrawal41>
2019-11-17 21:31:18 -0500

    b

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, redirect
 from search import search_all
 from general import credits_inv
+from datetime import datetime
 
 app = Flask(__name__)
-
-f = open("info/misc/search.txt", "a")
 
 @app.route('/',methods=['POST', 'GET'])
 def index():
@@ -17,6 +16,8 @@ def index():
         cr = request.form['cr']
         level = request.form['level']
         next_sem = request.form.getlist('next_sem')
+        with open("info/misc/search.txt", "a+") as f:
+            f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + "\t" + dep + "\t" + req + "\t" + sub + "\t" + code + "\t" + inst + "\t" + cr + "\t" + level + "\t" + str(next_sem) + "\n")
         all_courses = search_all(dep, sub, code, inst, req, level, cr, next_sem)
         if len(all_courses)>40:
             all_courses = all_courses[:40]
@@ -35,6 +36,8 @@ def output():
         cr = request.form['cr']
         level = request.form['level']
         next_sem = request.form.getlist('next_sem')
+        with open("info/misc/search.txt", "a+") as f:
+            f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + "\t" + dep + "\t" + req + "\t" + sub + "\t" + code + "\t" + inst + "\t" + cr + "\t" + level + "\t" + str(next_sem) + "\n")
         all_courses = search_all(dep, sub, code, inst, req, level, cr, next_sem)
         if len(all_courses)>40:
             all_courses = all_courses[:40]
@@ -54,6 +57,8 @@ def mobile():
         level = request.form['level']
         next_sem = request.form.getlist('next_sem')
         all_courses = search_all(dep, sub, code, inst, req, level, cr, next_sem)
+        with open("info/misc/search.txt", "a+") as f:
+            f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + "\t" + dep + "\t" + req + "\t" + sub + "\t" + code + "\t" + inst + "\t" + cr + "\t" + level + "\t" + str(next_sem) + "\n")
         if len(all_courses)>40:
             all_courses = all_courses[:40]
         return render_template('mobile_result.html', all_courses=all_courses, credits_inv=credits_inv)
@@ -72,6 +77,8 @@ def mobile_output():
         level = request.form['level']
         next_sem = request.form.getlist('next_sem')
         all_courses = search_all(dep, sub, code, inst, req, level, cr, next_sem)
+        with open("info/misc/search.txt", "a+") as f:
+            f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + "\t" + dep + "\t" + req + "\t" + sub + "\t" + code + "\t" + inst + "\t" + cr + "\t" + level + "\t" + str(next_sem) + "\n")
         if len(all_courses)>40:
             all_courses = all_courses[:40]
         return render_template('mobile_result.html', all_courses=all_courses, credits_inv=credits_inv)

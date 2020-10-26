e96b620e89f853f79f64d18316459158e574e3d2
Rishabh Agrawal <rishabhagrawal41@gmail.com>
2020-10-26 11:19:27 -0400

    Changed the url and how it looks

--- app.py
+++ app.py
@@ -11,20 +11,14 @@ app = Flask(__name__)
 @app.route('/',methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        keyword = str(request.form['keyword'])
-        dep = str(request.form['dept'])
-        req = str(request.form['requirement'])
-        sub = str(request.form['subject'])
-        code = str(request.form['code'])
-        inst = str(request.form['instrname'])
-        cr = str(request.form['credit'])
-        level = str(request.form['level'])
-        timing = str(request.form['timing'])
         days = list(map(str, request.form.getlist("day")))
-        next_sem = str(request.form['next_sem'])
+        search_query = dict(request.form)
+        search_query['days'] = days
+        search_query = str(search_query)[1:-1]
 
-        search_query = "&&".join([keyword, dep, req, sub, code, inst, cr, level, timing, str(days), next_sem])
-        search_query.replace(' ', '$')
+        search_query = search_query.replace(",", "&")
+        search_query = search_query.replace(":", "=")
+        search_query = search_query.replace(' ', '_')
         url = '/results&searchquery=' + search_query
         return redirect(url)
     else:
@@ -33,38 +27,37 @@ def index():
 @app.route('/results&searchquery=<query>', methods=['POST', 'GET'])
 def output(query):
     if query:
-        elements = query.split("&&")
-        keyword = elements[0].replace("$", " ")
-        dep = elements[1].replace("$", " ")
-        req = elements[2].replace("$", " ")
-        sub = elements[3].replace("$", " ")
-        code = elements[4].replace("$", " ")
-        inst = elements[5].replace("$", " ")
-        cr = elements[6].replace("$", " ")
-        level = elements[7].replace("$", " ")
-        timing = elements[8].replace("$", " ")
-        days = literal_eval(elements[9].replace("$", " "))
-        next_sem = elements[10].replace("$", " ")
+        query = query.replace("&", ",")
+        query = query.replace("=", ":")
+        query = query.replace('_', ' ')
+        elements = literal_eval("{" + query + "}")
+        print(elements)
+        keyword = elements.get("keyword")
+        dep = elements.get("dept")
+        req = elements.get("requirement")
+        sub = elements.get("subject")
+        code = elements.get("code")
+        inst = elements.get("instrname")
+        cr = elements.get("level")
+        level = elements.get("credit")
+        timing = elements.get("timing")
+        days = elements.get("days")
+        next_sem = elements.get("next sem")
+        print(next_sem)
         with open("info/misc/search.txt", "a+") as f:
             f.write(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + "\t" + dep + "\t" + req + "\t" + sub + "\t" + code + "\t" + inst + "\t" + cr + "\t" + level + "\t" + str(next_sem) + "\n")
         all_courses = search_all(dep, sub, code, inst, req, level, cr, next_sem, keyword, timing, days)
         if len(all_courses)>40:
             all_courses = all_courses[:40]
     if request.method == 'POST':
-        keyword = str(request.form['keyword'])
-        dep = str(request.form['dept'])
-        req = str(request.form['requirement'])
-        sub = str(request.form['subject'])
-        code = str(request.form['code'])
-        inst = str(request.form['instrname'])
-        cr = str(request.form['credit'])
-        level = str(request.form['level'])
-        timing = str(request.form['timing'])
         days = list(map(str, request.form.getlist("day")))
-        next_sem = (request.form['next_sem'])
+        search_query = dict(request.form)
+        search_query['days'] = days
+        search_query = str(search_query)[1:-1]
 
-        search_query = "&&".join([keyword, dep, req, sub, code, inst, cr, level, timing, str(days), next_sem])
-        search_query.replace(' ', '$')
+        search_query = search_query.replace(",", "&")
+        search_query = search_query.replace(":", "=")
+        search_query = search_query.replace(' ', '_')
         url = '/results&searchquery=' + search_query
         return redirect(url)
     else:
